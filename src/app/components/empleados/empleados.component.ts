b8cd4d6f3563462b15bc79eabfa07dad678dc657
Santiago9j <santitorresaguirre@gmail.com>
2024-09-11 10:09:37 -0500

    change forms to reactive-form angular

--- src/app/components/empleados/empleados.component.ts
+++ src/app/components/empleados/empleados.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableComponent } from "../table/table.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-empleados',
   standalone: true,
-  imports: [TableComponent],
+  imports: [TableComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './empleados.component.html',
   styleUrl: './empleados.component.css'
 })
-export class EmpleadosComponent {
+export class EmpleadosComponent implements OnInit {
+  empleadosForm!: FormGroup;
+  
+  ngOnInit(): void {
+      this.empleadosForm = new FormGroup({
+        dni : new FormControl('',[Validators.required]),
+        nombres : new FormControl('',[Validators.required]),
+        primerApellido : new FormControl('',[Validators.required]),
+        segundoApellido : new FormControl('',[Validators.required]),
+        fechaNacimiento : new FormControl('',[Validators.required]),
+        correo : new FormControl('',[Validators.required]),
+        estado : new FormControl('',[Validators.required])
+      })
+  }
 
+  submit(){
+    if (this.empleadosForm.valid){
+      console.log("Empleado listo")
+    }
+  }
 }
--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableComponent } from "../table/table.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [TableComponent],
+  imports: [TableComponent, ReactiveFormsModule,CommonModule],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  productsForm!: FormGroup
 
+  ngOnInit(): void {
+      this.productsForm = new FormGroup({
+        nombre: new FormControl('', [Validators.required]),
+        marca: new FormControl('', [Validators.required]),
+        costo: new FormControl('', [Validators.required]),
+        cantidadDisponible: new FormControl('', [Validators.required]),
+        estado: new FormControl('', [Validators.required])
+      });
+  }
+
+  submit (){
+    console.log("Hola productos")
+    if (this.productsForm.valid){
+      console.log("El formulario producto cumple con los requisitos")
+    }
+  }
 }
